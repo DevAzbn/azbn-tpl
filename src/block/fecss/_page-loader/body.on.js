15d8f6f15f03dbf9fbf0404e6e98772e071377a3
Alexander <alexander.zybin@gmail.com>
2016-08-29 20:54:53 +0300

    201608292054

--- src/block/fecss/_page-loader/body.on.js
+++ src/block/fecss/_page-loader/body.on.js
@@ -5,4 +5,8 @@ $(window).on('load', function(event){
 	
 	$('.page-loader').removeClass('active').empty().remove();
 	
+	$('.page-container').addClass('smooth-animation');
+	$('.page-container .fluid-block').addClass('smooth-item');
+	$('.smooth-animation').attr('data-state', 'active');
+	
 });
