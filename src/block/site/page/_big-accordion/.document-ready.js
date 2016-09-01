18ea8ce012dd5f6ae7140f8d42a88cb19a9c217b
Alexander <alexander.zybin@gmail.com>
2016-09-01 09:52:57 +0300

    201609010952

---
+++ src/block/site/page/_big-accordion/.document-ready.js
@@ -0,0 +1,40 @@
+
+$(document.body).on('click.site.big-accordion.surface.surface-btn', '.big-accordion .surface .surface-btn', {}, function(event){
+	event.preventDefault();
+	
+	var btn = $(this);
+	var href = btn.attr('href');
+	var surface = btn.closest('.surface');
+	var content = surface.find(href + '.surface-content');
+	var state = surface.attr('data-state');
+	
+	switch(state) {
+		
+		case 'default':{
+			surface.attr('data-state', 'active');
+			content.slideDown('fast');
+		}
+		break;
+		
+		case 'active':{
+			surface.attr('data-state', 'default');
+			content.slideUp('fast');
+		}
+		break;
+		
+		default:{
+			
+		}
+		break;
+		
+	}
+	
+});
+
+
+$(function(){
+	
+	$('.big-accordion .surface > .surface-content').hide();
+	$('.big-accordion .surface').attr('data-state', 'default');
+	
+})
