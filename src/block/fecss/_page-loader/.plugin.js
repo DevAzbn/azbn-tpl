94885029cc8597052b01724c26f958362df2e83a
Alexander <alexander.zybin@gmail.com>
2016-09-07 14:41:17 +0300

    201609071441

--- src/block/fecss/_page-loader/.plugin.js
+++ src/block/fecss/_page-loader/.plugin.js
@@ -4,7 +4,7 @@ $(window).on('load', function(event){
 	console.log('window trigger:load');
 	
 	$('body').removeClass('overflow-hidden');
-	$('.page-loader').removeClass('active').empty().remove();
+	$('.page-loader').removeClass('active');//.empty().remove();
 	
 	$('.page-container').addClass('smooth-animation');
 	$('.page-container .fluid-block').addClass('smooth-item');
--- src/block/site/page/_bottom-menu-ul/.document-ready.js
+++ src/block/site/page/_bottom-menu-ul/.document-ready.js
@@ -1,7 +1,8 @@
-
+/*
 $(document.body).on('click.site', '.bottom-menu-ul li a', {}, function(event){
 	event.preventDefault();
 	
 	$(this).closest('.bottom-menu-ul').find('li').removeClass('active');
 	$(this).closest('li').addClass('active');
 });
+*/
